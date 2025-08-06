89cb11a2260dd3643e91cced0eda0a6dbaf0269f
ht-oliver <holiver327@gmail.com>
2025-08-05 22:37:29 -0700

    Added comments, hopefully this will come in useful one time.

--- Graph_Practice_Script.R
+++ Graph_Practice_Script.R
@@ -5,22 +5,30 @@
 #usethis::use_git()
 #usethis::use_github()
 
+## Define your function
 f_x <- function(x) {
   ((3*x^2 - 2*x + 3)^2)
   }
 
+## Define your data range
 my_data_range <- data.frame(x = c(-20, 20))
 
+
+## define your object that is a derivative
 df_dx <- D(expr = expression(
   ((3*x^2 - 2*x + 3)^2)),
   name = "x")
 
+## return the derivative of your input function from the console
+
 df_dx
 
+# take the returned derivative from the console, and determine a new function
 df_dx_fun <- function(x) {
   2 * ((3 * (2 * x) - 2) * (3 * x^2 - 2 * x + 3))
 }
 
+#plot both functions
 ggplot(data = my_data_range, aes(x = x)) +
   geom_function(fun = f_x, color = "orange") +
   geom_function(fun = df_dx_fun, color = "blue")
