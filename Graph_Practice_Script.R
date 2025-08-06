9b69ece5e85d7338d0cda858d6ec650e342d82e1
ht-oliver <holiver327@gmail.com>
2025-08-05 21:29:29 -0700

    Initial save commit

---
+++ Graph_Practice_Script.R
@@ -0,0 +1,8 @@
+## The goal of this code is to plot a function and the derivative of that function. Comments with a single "#" are lines of code that should be run in the console before executing the main script.
+## The function shall be f(x) = (x^2 + 2x - 4.5) + 17
+# library(tidyverse)
+
+#usethis::use_git()
+#usethis::use_github()
+
+fx <- expression((x^2 + 2x -4.5)+17)
