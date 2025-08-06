65ed86e93de5419b6f7d3f0080558aca7faed33d
ht-oliver <holiver327@gmail.com>
2025-08-05 22:06:37 -0700

    First functional graph

--- Graph_Practice_Script.R
+++ Graph_Practice_Script.R
@@ -1,8 +1,20 @@
 ## The goal of this code is to plot a function and the derivative of that function. Comments with a single "#" are lines of code that should be run in the console before executing the main script.
-## The function shall be f(x) = (x^2 + 2x - 4.5) + 17
+
 # library(tidyverse)
 
 #usethis::use_git()
 #usethis::use_github()
 
-fx <- expression((x^2 + 2x -4.5)+17)
+f_x <- function(x) {((x^2 + 2*x - 14)^2 +1)}
+idiot <- function(n) {sin(n)}
+
+
+x <- seq(from = -20, to = 20, by = 1)
+
+ggplot() + xlim(c(-20,20)) + geom_function(fun = f_x)
+
+
+
+
+
+
